663bd6c376f1ca964b33a9b9171e776f3f2a62ff
Laure <laure.fesler@gmail.com>
2024-01-26 15:28:29 +0100

    m

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/year', (req, res) => {
+  const date = new Date()
+  const year = date.getFullYear().toString();
+  res.json({ year: date });
+ });
+
+module.exports = router;
